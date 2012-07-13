85b13027cb1ad37157c6d90ef1090f00b196672b
Kasper Munch <kaspermunch@birc.au.dk>
2012-07-13 10:07:37 +0000

    Fixed a bug so we now test sorted lists of species names.

--- CoalhmmPipeline/MafTest.py
+++ CoalhmmPipeline/MafTest.py
@@ -3,7 +3,7 @@
 class MafTest(object):
 
     def __init__(self, speciesList):
-        self.speciesList = speciesList
+        self.speciesList = sorted(speciesList)
 
     def test(self, maf):
         return self.speciesList == [maf.name(i) for i in range(maf.count())]
