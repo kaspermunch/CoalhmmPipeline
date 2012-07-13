f3042ad26a8efc3a0c6f70eb13ac32c10148b24b
Kasper Munch <kaspermunch@birc.au.dk>
2012-07-13 10:01:43 +0000

    Added some print statements

--- CoalhmmPipeline/Chunkenizer.py
+++ CoalhmmPipeline/Chunkenizer.py
@@ -42,10 +42,27 @@ class Chunkenizer:
 		toMerge = [] #list of mafs to merge
 		q = deque() #queue containing mafs which still need processing
 		mafFile = open(inputMAF) 
+
+# 		TEST_print = False
 		
 		for maf in MAFIterator(mafFile):
 
+# 			print "INPUT"
+# 			print maf
+# 			assert all(maf.srcLength(i) > 0 for i in range(maf.count()))
+# 			for i in range(len(maf.alignment)):
+# 				_, TEST_name, TEST_start, TEST_length, _, _, _ = maf.alignment[i]
+# 				if TEST_name == "Hsap" and TEST_start > 0.97e8:
+# 					TEST_print = True
+# 			if TEST_print:
+# 				raw_input("press enter")
+# 				print "Input MAF"
+# 				print maf
+
+
 			if not self.mafTest.test(maf):
+  				print 'MAF entry failed test:'
+  				print maf
 				continue
 
 			#split maf and put it on the processing que
@@ -91,7 +108,20 @@ class Chunkenizer:
 				else:
 				    if self.acceptChunk(toMerge):
     					output.writeChunk(toMerge)
+
+# 					for m in toMerge:
+# 						print "OUTPUT"
+# 						print m
+# 						assert all(m.srcLength(i) > 0 for i in range(m.count()))
+# 
+# 					if TEST_print:
+# 						for m in toMerge:
+# 							print "Output MAF"
+# 							print m
+# 
 					toMerge = [e]
+
+
 		
 		#since the tomerge list is never emty we need to write out the last chunk
 		if len(toMerge) > 0 and self.acceptChunk(toMerge):
