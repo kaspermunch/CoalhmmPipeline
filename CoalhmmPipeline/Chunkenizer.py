cfdb1f5be902418a518dee5db1b0d6b0a53eb373
Kasper Munch <kaspermunch@birc.au.dk>
2013-01-17 09:39:57 +0000

    Cleaned up comments a bit

--- CoalhmmPipeline/Chunkenizer.py
+++ CoalhmmPipeline/Chunkenizer.py
@@ -11,127 +11,92 @@
 #)
 #WARNING: Use instances only once! depending parameter objects data may be corrupted
 #TODO: consider making a use once or just a function
+import sys
 from MAF import *
 from collections import deque
+from PytablesOutput import CoordinateError
 
 class Chunkenizer:
-	def __init__(self, splitter, mergeCriteria, mafTest, mafQualityFilters, chunkQualityFilters, truncater):
-		self.mafTest = mafTest
-		self.splitter = splitter
-		self.mergeCriteria = mergeCriteria
-		self.mafQualityFilters = mafQualityFilters
-		self.chunkQualityFilters = chunkQualityFilters
-		self.truncater = truncater
-	
-	def acceptMaf(self, maf):
-		for mqf in self.mafQualityFilters:
-			if not mqf.accept(maf): 
-				return False
-		
-		return True
-		
-	def acceptChunk(self, mafs):
-		for cqf in self.chunkQualityFilters:
-			if not cqf.accept(mafs): 
-				return False
-		
-		return True
-		
-	def chunkenize(self, inputMAF, output):
 
-		toMerge = [] #list of mafs to merge
-		q = deque() #queue containing mafs which still need processing
-		mafFile = open(inputMAF) 
+    def __init__(self, splitter, mergeCriteria, mafTest, mafQualityFilters, chunkQualityFilters, truncater):
+        self.mafTest = mafTest
+        self.splitter = splitter
+        self.mergeCriteria = mergeCriteria
+        self.mafQualityFilters = mafQualityFilters
+        self.chunkQualityFilters = chunkQualityFilters
+        self.truncater = truncater
+	self.instanceUsedAlready = False
+    
+    def acceptMaf(self, maf):
+        for mqf in self.mafQualityFilters:
+            if not mqf.accept(maf): 
+                return False		
+        return True
+    	
+    def acceptChunk(self, mafs):
+        for cqf in self.chunkQualityFilters:
+            if not cqf.accept(mafs): 
+                return False		
+        return True
+    	
+    def chunkenize(self, inputMAF, output):
 
-# 		TEST_print = False
-		
-		for maf in MAFIterator(mafFile):
+        assert not self.instanceUsedAlready, "WARNING: Use instances only once! depending parameter objects data may be corrupted"
+        self.instanceUsedAlready = True
 
-# 			print "INPUT"
-# 			print maf
-# 			assert all(maf.srcLength(i) > 0 for i in range(maf.count()))
-# 			for i in range(len(maf.alignment)):
-# 				_, TEST_name, TEST_start, TEST_length, _, _, _ = maf.alignment[i]
-# 				if TEST_name == "Hsap" and TEST_start > 0.97e8:
-# 					TEST_print = True
-# 			if TEST_print:
-# 				raw_input("press enter")
-# 				print "Input MAF"
-# 				print maf
-
-
-			if not self.mafTest.test(maf):
-#  				print maf
-				continue
-
-			#split maf and put it on the processing que
-			for splitmaf in self.splitter.split(maf):
-				#truncate the maf
-				#print "before truncating\n", splitmaf, "\nafter"
-				splitmaf = self.truncater.truncate(splitmaf)
-				#print splitmaf
-				#raw_input("press enter")    
-				
-				if splitmaf != None and self.acceptMaf(splitmaf):
-					q.append(splitmaf)
-			
-
-			if len(q) == 0: #queue is empty get the next
-			    continue
-
-			"""if len(self.splitter.split(maf)) > 1:
-				print "this\n", maf, "\nsplitinto"
-				for splitmaf in self.splitter.split(maf):
-					print splitmaf
-					
-				print "end split"
-				raw_input("press enter")"""
-				
-			
-			#if the tomerge list is empty then just add one maf from the queue
-			#later code assumes toMerge is not empty
-			if len(toMerge) == 0:
-				toMerge.append(q.popleft())
-			
-			#now just flush the queue
-			#for every element in the queue: test with the mergeCriteria against the last added maf in tomerge list
-			#if they should merge: just add the element to the tomerge list
-			#otherwise writeout the tomerge list, and then add.
-			
-			while len(q) > 0:
-				e = q.popleft()
-				last = toMerge[len(toMerge) -1]
-				
-				if self.mergeCriteria.shouldMerge(last, e):
-					toMerge.append(e)
-				else:
-				    if self.acceptChunk(toMerge):
-    					output.writeChunk(toMerge)
-
-# 					for m in toMerge:
-# 						print "OUTPUT"
-# 						print m
-# 						assert all(m.srcLength(i) > 0 for i in range(m.count()))
-# 
-# 					if TEST_print:
-# 						for m in toMerge:
-# 							print "Output MAF"
-# 							print m
-# 
-					toMerge = [e]
-
-
-		
-		#since the tomerge list is never emty we need to write out the last chunk
-		if len(toMerge) > 0 and self.acceptChunk(toMerge):
-			output.writeChunk(toMerge)			
-		
-		#fullfill the output objects last wishes... if any
-		output.finalize()
-
-
-
-#example use		
-#ingroup = ["hg18", "pantro2", "bonobo"]
-#chunkenizer = Chunkenizer(Splitter(), MergeCriteria(kmerge, ingroup), Output("/tmp/chunks", ingroup, "/tmp/hdf.h5"))
-#chunkenizer.chunkenize(inputFile)
+        toMerge = [] #list of mafs to merge
+        q = deque() #queue containing mafs which still need processing
+        mafFile = open(inputMAF) 
+        
+        for maf in MAFIterator(mafFile):
+                
+            if not self.mafTest.test(maf):
+            	continue
+            
+            #split maf and put it on the processing que
+            for splitmaf in self.splitter.split(maf):
+            	#truncate the maf
+            	#print "before truncating\n", splitmaf, "\nafter"
+            	splitmaf = self.truncater.truncate(splitmaf)
+            	#print splitmaf
+            	#raw_input("press enter")    
+            	
+            	if splitmaf != None and self.acceptMaf(splitmaf):
+            		q.append(splitmaf)
+            
+            
+            if len(q) == 0: #queue is empty get the next
+                continue
+            
+            #if the tomerge list is empty then just add one maf from the queue
+            #later code assumes toMerge is not empty
+            if len(toMerge) == 0:
+            	toMerge.append(q.popleft())
+            
+            #now just flush the queue
+            #for every element in the queue: test with the mergeCriteria against the last added maf in tomerge list
+            #if they should merge: just add the element to the tomerge list
+            #otherwise writeout the tomerge list, and then add.
+            
+            while len(q) > 0:
+            	e = q.popleft()
+            	last = toMerge[len(toMerge) -1]
+            	
+            	if self.mergeCriteria.shouldMerge(last, e):
+		    toMerge.append(e)
+            	else:
+            	    if self.acceptChunk(toMerge):
+            	        try:
+			    output.writeChunk(toMerge)
+			except CoordinateError, err:
+			    print >>sys.stderr, err.message
+			toMerge = [e]
+        
+        
+        
+        #since the tomerge list is never emty we need to write out the last chunk
+        if len(toMerge) > 0 and self.acceptChunk(toMerge):
+        	output.writeChunk(toMerge)			
+        
+        #fullfill the output objects last wishes... if any
+        output.finalize()
